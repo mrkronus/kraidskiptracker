0512ec885cc84fa5cdac7a063639b97f0b6afabe
MrKronus <kronus@kronus.com>
2024-04-07 22:09:23 -0700

    Adding Ace3 libs and a single options to the Addon settings

--- main.lua
+++ main.lua
@@ -8,6 +8,44 @@ local addonName, KRaidSkipTracker = ...
 --
 local LibQTip = LibStub('LibQTip-1.0')
 local LibDataBroker = LibStub("LibDataBroker-1.1")
+local LibAceAddon = LibStub("AceAddon-3.0"):NewAddon("KRaidSkipTracker", "AceConsole-3.0", "AceEvent-3.0")
+
+local aceOptions = {
+    name = "KRaidSkipTracker",
+    handler = LibAceAddon,
+    type = 'group',
+    args = {  
+        showNotStarted = {
+            type = "toggle",
+            name = "Show Not Started Skips",
+            desc = "Toggles the display of progression for skips that have not been started yet",
+            get = "ShouldShowNotStarted",
+            set = "ToggleShowNotStarted",
+        },
+    },
+}
+
+local aceOptionsDefaults = {
+    profile =  {
+        showNotStarted = true,
+    },
+}
+
+function LibAceAddon:ShouldShowNotStarted(info)
+    return self.db.profile.showNotStarted
+end
+
+function LibAceAddon:ToggleShowNotStarted(info, value)
+    self.db.profile.showNotStarted = value
+end
+
+LibAceAddon.showNotStarted = true
+
+function LibAceAddon:OnInitialize()
+    self.db = LibStub("AceDB-3.0"):New("KRaidSkipTrackerDB", aceOptionsDefaults, true)
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("KRaidSkipTracker", aceOptions, {"kraidskiptracker", "krst"})
+    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("KRaidSkipTracker", "KRaidSkipTracker")
+end
 
 function tip_OnClick(clickedframe, button)
     if button == "RightButton" then
@@ -54,7 +92,9 @@ local function tip_OnEnter(self)
             if v.isStatistic == true then
                 local statisticVal = GetStatistic(v.questId)
                 if(statisticVal == nil or statisticVal == "--" or tonumber(statisticVal) == 0) then
-                    tooltip:AddLine(format("%s:", v.keyName), format("Incomplete"))
+                    if LibAceAddon:ShouldShowNotStarted() then
+                        tooltip:AddLine(format("%s:", v.keyName), format("\124cFFA9A9A9Incomplete\124r"))
+                    end
                 else
                     tooltip:AddLine(format("%s:", v.keyName), format("\124cff00ff00Acquired\124r"))
                 end
@@ -63,8 +103,43 @@ local function tip_OnEnter(self)
                 if questValue == true then
                     tooltip:AddLine(format("%s:", v.keyName), format("\124cff00ff00Acquired\124r"))                    
                 else
-                    local questText, questObjectiveType, isFinished, numFulfilled, numRequired = GetQuestObjectiveInfo(v.questId, 1, false)
-                    tooltip:AddLine(format("%s:", v.keyName), format("%i/%i", numFulfilled, numRequired))     
+                    local questObjectives = C_QuestLog.GetQuestObjectives(v.questId)
+                    local hasStartedAnyObjective = false
+                    if questObjectives then
+                        for _,objective in ipairs(questObjectives) do
+                            if objective then
+                                if objective.numFulfilled > 0 then
+                                    hasStartedAnyObjective = true
+                                    break
+                                end
+                            end
+                        end
+                    end
+
+                    if hasStartedAnyObjective then
+                        local objectiveIndex = 1
+                        local objectivesString = "(none)"
+                        for _,objective in ipairs(questObjectives) do
+                            if objective then
+
+                                local questText, questObjectiveType, isFinished, numFulfilled, numRequired = GetQuestObjectiveInfo(v.questId, objectiveIndex, false)
+                                if objectiveIndex == 1 then
+                                    objectivesString = format("%i/%i", numFulfilled, numRequired)
+                                else
+                                    objectivesString = objectivesString .. format(" | %i/%i", numFulfilled, numRequired)
+                                end
+
+                                objectiveIndex = objectiveIndex + 1
+                            end
+
+                        end
+
+                        tooltip:AddLine(format("%s:", v.keyName), objectivesString)
+                    else 
+                        if LibAceAddon:ShouldShowNotStarted() then
+                            tooltip:AddLine(format("%s:", v.keyName), format("\124cFFA9A9A9Not Started\124r"))
+                        end
+                    end
                 end
             end
         end
@@ -81,6 +156,9 @@ local function tip_OnLeave(self)
     self.tooltip = nil
 end
 
+--
+-- LibDBIcon initialization
+--
 local dataobj = LibDataBroker:NewDataObject("K Keyed", {
     type = "launcher",
     icon = [[Interface/Icons/Inv_misc_key_15]],
@@ -89,9 +167,6 @@ local dataobj = LibDataBroker:NewDataObject("K Keyed", {
     OnEnter = tip_OnEnter
 })
 
---
--- LibIcon initialization
---
-local icon = LibStub("LibDBIcon-1.0", true)
-icon:Register("K Keyed", dataobj, KKeyedDB)
-icon:Show("K Keyed")
+local libIcon = LibStub("LibDBIcon-1.0", true)
+libIcon:Register("K Keyed", dataobj, KKeyedDB)
+libIcon:Show("K Keyed")
